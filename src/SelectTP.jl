573852c8aa2369250cca84f93ca5923e1b34221b
yakir12 <12.yakir@gmail.com>
2020-06-21 18:14:27 +0200

    button

--- src/SelectTP.jl
+++ src/SelectTP.jl
@@ -7,17 +7,19 @@ using AbstractPlotting, GLMakie, MakieLayout, Colors
 export selectTP
 
 function selectTP(file = "data")
+
     data = deserialize(file)
     scene, layout = layoutscene()
     ax = layout[1, 1] = LAxis(scene, aspect = DataAspect())
     track = Node(rand(DungBase.Point, 10))
     feeder = Node(rand(DungBase.Point, 1))
     lines!(ax, track)
-    scatter!(ax, feeder, color = :green, markersize = 2)
-    mousestate = addmousestate!(ax.scene)
+    scatter!(ax, feeder, color = :green, markersize = 8px)
     tp1 = Node(rand(2))
+    mousestate = addmousestate!(ax.scene)
     onmouseover(mousestate) do state
         tp1[] = state.pos
+        autolimits!(ax)
     end
     tp = map(tp1) do xy
         _, i = findmin(norm.(track[] .- Ref(xy)))
@@ -26,24 +28,26 @@ function selectTP(file = "data")
     tp2 = map(tp) do i
         [track[][i]]
     end
-    scatter!(ax, tp2, color = RGBA(1,0,0,0.5), markersize = 3)
-    c = Condition()
+    scatter!(ax, tp2, color = RGBA(1,0,0,0.5), markersize = 10px)
     onmouseleftclick(mousestate) do state
-        i = tp[]
-        @info "turning point index: $i"
+        println(string(r.metadata.comment, ",", tp[]))
+    end
+    c = Condition()
+    h = layout[2,1] = LButton(scene, label = "Next!", tellwidth = false)
+    on(h.clicks) do _
         notify(c)
     end
     for (k, v) in data
         @info "doing experiment: $k"
         for r in v.runs
-            @info "doing run: $(r.metadata.comment)"
             track[] = r.data.track.coords
             feeder[] = [r.data.feeder]
-            autolimits!(ax)
             display(scene)
             wait(c)
         end
     end
+
 end
+selectTP()
 
 end
